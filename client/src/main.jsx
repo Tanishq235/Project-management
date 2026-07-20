7e3054acc70acdb3acebba29f6a6b992c2b5c8ec
Tanishq kadam <Tanishqkadam23@gmail.com>
2026-07-20 15:25:23 +0530

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+import {ClerkProvider} from '@clerk/react';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
+
+createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+            <Provider store={store}>
+                <ClerkProvider afterSignOutUrl="/">
+      <App />
+    </ClerkProvider>
+            </Provider>
+    </BrowserRouter>,
+)
