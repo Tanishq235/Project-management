430d73811086e43c31ff763f6df4cd9e71aaa1c4
Tanishq kadam <Tanishqkadam23@gmail.com>
2026-07-22 17:08:08 +0530

    changed s in adress

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -10,7 +10,7 @@ const syncUserCreation = inngest.createFunction({id:'sync-user-from-clerk'},{eve
     await prisma.user.create({
       data:{
         id:data.id,
-        email:data.email_addresses[0]?.email_addres,
+        email:data.email_addresses[0]?.email_address,
         name:data?.first_name+" "+data?.last_name,
         image:data?.image_url,
       }
